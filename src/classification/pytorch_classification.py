fba45229c2a45aa935d73f184ddf0a09c573fe41
shyamprasath <shyamprasath@detecttechnologies.com>
2023-05-25 22:58:55 -0400

    Added img size

--- src/classification/pytorch_classification.py
+++ src/classification/pytorch_classification.py
@@ -20,18 +20,19 @@ class Image_classification:
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print(f"The process is running in {self.device}")
 
-    def data_transform(self, train_data_dir, val_data_dir):
+    def data_transform(self, train_data_dir, val_data_dir, img_size):
         # Transform the data
         transform_data = {
             "train": ImageFolder(root= train_data_dir,
                                     transform= transforms.Compose([
+                                    transforms.Resize(img_size),
                                     transforms.RandomResizedCrop(640),
                                     transforms.RandomHorizontalFlip(),
                                     transforms.ToTensor()])
             ),
             "val": ImageFolder(root= val_data_dir,
                                     transform=transforms.Compose([
-                                    transforms.Resize(640),
+                                    transforms.Resize(img_size),
                                     transforms.CenterCrop(640),
                                     transforms.ToTensor()])
             )
@@ -43,8 +44,8 @@ class Image_classification:
         
         # Load the data
         data_loader = {
-            "train": DataLoader(data_transform["train"], batch_size=batch_size, shuffle=True, num_workers=2),
-            "val": DataLoader(data_transform["val"], batch_size=batch_size, shuffle=True, num_workers=2)
+            "train": DataLoader(data_transform["train"], batch_size=batch_size, shuffle=True, num_workers=4),
+            "val": DataLoader(data_transform["val"], batch_size=batch_size, shuffle=True, num_workers=4)
         }
         return data_loader
 
@@ -73,7 +74,7 @@ class Image_classification:
             print(f"Epoch {epoch}/{epochs-1}")
             print('*'*50)
 
-            for phase in ["train", "test"]:
+            for phase in ["train", "val"]:
                 training = phase == "train"
 
                 if training:
@@ -114,22 +115,28 @@ class Image_classification:
                 
 
 if __name__=='__main__':
-    train_data_dir = "/Volumes/Projects/cv_suite/data/train"
-    val_data_dir = "/Volumes/Projects/cv_suite/data/test"
-    batch_size = 4
+    train_data_dir = "/home/shyam/Documents/CV suite/data/train"
+    val_data_dir = "/home/shyam/Documents/CV suite/data/test"
+    img_size = 640,640
+    batch_size = 12
     epochs = 10
     model = "resnet18"
     freeze_layer = True
 
+
     image_classification = Image_classification()
-    data_transform = image_classification.data_transform(train_data_dir, val_data_dir)
+    data_transform = image_classification.data_transform(train_data_dir, val_data_dir, img_size)
     print("Data transformed")
+
     data_loader = image_classification.data_loader(data_transform, batch_size)
     print("Data Loader created")
+
     model = image_classification.build_model(model, data_transform, freeze_layer)
     print("Model built successfully")
+
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+
     model = image_classification.train_loop(model, data_loader, loss_fn, optimizer, epochs)
     print("Model trained successfully")
 
